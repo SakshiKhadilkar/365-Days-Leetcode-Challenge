ca4a28e8b2be9c89842419dae6135b4f5dbe7a7c
Sakshi Khadilkar <117538804+SakshiKhadilkar@users.noreply.github.com>
2024-01-18 18:43:45 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (20.04%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        
+       if(str1 + str2 != str2 + str1)
+           return "";
+         
+        int n1 = str1.size();
+        int n2 = str2.size();
+        
+       return str1.substr(0,__gcd(n1,n2));
+    }
+};

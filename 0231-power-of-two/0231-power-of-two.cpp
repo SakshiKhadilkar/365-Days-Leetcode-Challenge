5b62dcd7e972cbb21833c0e92627bba90f743f12
Sakshi Khadilkar <117538804+SakshiKhadilkar@users.noreply.github.com>
2024-02-19 21:07:11 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (39.77%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -2,9 +2,12 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         
-        if(n <= 0)
+        if(n<=0)
             return false;
         
-        return (n&(n-1)) == 0;
+        if(n==1)
+            return true;
+        
+        return n%2 == 0 && isPowerOfTwo(n/2);
     }
 };
